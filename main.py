687cb26cb8541e1984a80ee91ec651b5988d346c
Elisha Hollander <just4now666666@gmail.com>
2021-03-15 02:11:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -206,24 +206,23 @@
 
 
 
-import curses;from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN;from random import randint;curses.initscr();win=curses.newwin(20, 60, 0, 0);win.keypad(1);curses.noecho();curses.curs_set(0);win.border(0);win.nodelay(1);key=KEY_RIGHT;score=0;snake=[[4,10], [4,9], [4,8]];food=[10,20];win.addch(food[0], food[1], '*')
-while True:
-    win.border(0);win.addstr(0,2,'Score:'+str(score)+' ');win.addstr(0,27,' SNAKE ');win.timeout(150-int(len(snake)/5 + len(snake)/10)%120);prevKey=key;event=win.getch();key=key if event==-1 else event
-    if key==ord(' '):
-        key = -1
-        while key != ord(' '):key = win.getch()
-        key = prevKey;continue
-    if key not in[KEY_LEFT,KEY_RIGHT,KEY_UP,KEY_DOWN,27]:key=prevKey
-    snake.insert(0,[snake[0][0]+(key==KEY_DOWN and 1)+(key==KEY_UP and -1),snake[0][1]+(key==KEY_LEFT and -1)+(key==KEY_RIGHT and 1)])
-    if snake[0][0] == 0: snake[0][0] = 18
-    if snake[0][1] == 0: snake[0][1] = 58
-    if snake[0][0] == 19: snake[0][0] = 1
-    if snake[0][1] == 59: snake[0][1] = 1
-    if snake[0] in snake[1:]:break
-    if snake[0] == food:
-        food = [];score += 1
-        while food == []:food = [randint(1, 18), randint(1, 58)] if food not in snake else []
-        win.addch(food[0], food[1], '*')
-    else:last = snake.pop();win.addch(last[0], last[1], ' ')
-    win.addch(snake[0][0], snake[0][1], '#')
-curses.endwin()
+from curses import *
+import time
+from collections import deque
+from random import randrange as R
+N,W,S,E=z=119,97,115,100
+t=tuple
+u=0,0
+def m(s):
+ a=lambda o,y,x:y.addch(o[0],o[1],x);q=lambda:(R(L-2),R(C-2));L,C=s.getmaxyx();curs_set(0);s.nodelay(1);s.border();s.refresh();r=newwin(L-2,C-2,1,1);n=deque();y,x=[L-2,0];d=N;n.append(u);c=N;p=q();a(p,r,N);a(u,s,48)
+ while 1:
+  if c in z:d=c
+  if d==N:y-=1
+  if d==S:y+=1
+  if d==W:x-=1
+  if d==E:x+=1
+  l=n.pop()
+  if (y,x) in n:return
+  if (y,x)==p:p=q();a(p,r,N);n.append(l);s.addstr(0,0,str(len(n)))
+  n.appendleft((y,x));a((y,x),r,S);a(l,r,32);r.refresh();time.sleep(.2);c=s.getch()
+wrapper(m)
